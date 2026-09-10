4909e248ebceda7f1d6eee3584dfc8ee50c4472e
Prateek Yashaswi <60723631+Prateek-Yashaswi@users.noreply.github.com>
2026-09-10 12:18:56 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        return solve(piles, h);
+    }
+
+    private static int solve(int[] piles, int h) {
+        var left = 1;
+        int right = Arrays.stream(piles).boxed().max(Comparator.naturalOrder()).orElseThrow();
+
+        while (left <= right) {
+            var k = left + (right - left) / 2;
+            var hoursNeeded = 0;
+
+            for (var item : piles) {
+                hoursNeeded += (int) Math.ceil((double) item / k); // (int) Math.ceil((double) item / k)
+            }
+
+            if (hoursNeeded <= h) {
+                right = k - 1;
+            } else {
+                left = k + 1;
+            }
+        }
+
+        return left;
+    }
+}
